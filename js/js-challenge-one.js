830ecfbb42cc282262feb514e56d6ffa994a2571
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-22 21:49:06 -0800

    completed exercise 1 and 2; everything executed fine except there was a weird error message about the integrity of the bootstrap.min.js; will come back to fix this later

---
+++ js/js-challenge-one.js
@@ -0,0 +1,21 @@
+// Exercise 1
+const fullName = "Elizabeth-Agnes Velez Gaw";
+const welcomeMessage = ", welcome!"
+const fullMessage = fullName + welcomeMessage;
+
+alert(fullMessage);
+
+// Exercise 2
+
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+const num5 = 5;
+const num6 = 6;
+
+const answer1 = num1 + num2 + num3 + num4 + num5 + num6;
+console.log(answer1);
+
+const answer2 = (num1 + num2 + num3) * num4;
+console.log(answer2);
