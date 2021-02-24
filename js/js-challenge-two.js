9976624dcc305e9f8f00dd5e3a72a14f97da7642
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-23 17:58:03 -0800

    made for loop to print odd numbers from 0-100; created function that takes in firstName and lastName and prints out the welcome message with the full name from first and last name

---
+++ js/js-challenge-two.js
@@ -0,0 +1,15 @@
+// Exercise 1
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    } 
+}
+
+// Exercise 2
+
+const welcome = (firstName, lastName) => {
+    alert(firstName + " " + lastName + ", welcome!");
+}
+
+welcome("Elizabeth-Agnes", "Gaw");
