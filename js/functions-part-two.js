fe9e3c83e22243eab895afe15be70220cebfa7a0
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-25 15:07:27 -0800

    added another function to verify if number is NaN

---
+++ js/functions-part-two.js
@@ -0,0 +1,35 @@
+const guessNumber = () => {
+
+    const correctNumber = Math.floor(Math.random() * 10 + 1);
+
+    // debugger;
+
+    for (let i = 0; i <= 2; i ++) {
+        let userInput = prompt('Guess a number from 1 to 10. You have 3 guesses.');
+
+        const verified = verifyNumber(userInput);
+        // debugger;
+        if (verified) {
+            alert('That is not a number!');
+        } else if (userInput == correctNumber) {
+            alert('Correct! You win!');
+            break;
+        } else {
+            alert('Wrong, try again!');
+        }
+
+        if (i === 2) alert('You are out of guesses!');
+
+    }
+
+}
+
+const verifyNumber = (userInput) => {
+
+    const result = isNaN(userInput);
+
+    // debugger;
+
+    return result;
+
+}
