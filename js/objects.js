af1003a74a3e21b9dabc3bc0a7dc7d4c4b6adb80
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-03-01 10:13:54 -0800

    added code for morning challenge; instruction for challenge in file

--- js/objects.js
+++ js/objects.js
@@ -124,3 +124,59 @@ let evilAnimalNames = animals.filter((animal) => {
 });
 
 console.log('evil animal names:', evilAnimalNames);
+
+// =================== Morning Challenge 3/1/2021 =========================
+
+let items = [
+    { name: 'Huffy X27', price: 100, category: 'fitness' },
+    { name: 'The Alchemist', price: 15, category: 'books' },
+    { name: 'Snuggy', price: 10, category: 'home' },
+    { name: 'Macbook Pro', price: 1500, category: 'technology' },
+    { name: 'Peloton', price: 2000, category: 'fitness' },
+    { name: 'iPad Pro', price: 800, category: 'technology' },
+    { name: 'Ember Coffee Mug', price: 80, category: 'home' },
+    { name: 'Electric Wine Bottle Opener', price: 20, category: 'home' },
+  ];
+
+// Loop through the items, console.log the price with a concatenated string that let's the user know the total price
+
+let totalPrice = 0;
+
+for (let key in items) {
+    totalPrice += items[key].price;
+}
+
+console.log('The total price of all items together is $', totalPrice);
+
+
+// Create a new array using the map method, that returns all the names of each item
+
+let itemNames = items.map(item => {
+    return item.name;
+});
+
+console.log('Item names:', itemNames);
+
+// Create a new array using the filter method that returns all the items that are less than 100
+
+let under100DollarItems = items.filter(item => {
+    return item.price < 100; // the filtering condition
+});
+
+console.log('Items under $100:', under100DollarItems);
+
+// Using the find method loop through the array and return the laptop item setting it to a variable
+
+const laptop = items.find(item => item.name === 'Macbook Pro');
+
+console.log(laptop);
+
+// Chaining the filter and map method together, loop through the items, filter by items that are more than 100, then using the map method, create a new array of the names of those items
+
+let over100DollarItems = items.filter((item) => {
+    return item.price > 100;
+}).map((over100DollarItem) => {
+    return over100DollarItem.name;
+});
+
+console.log('Items over $100:', over100DollarItems);
